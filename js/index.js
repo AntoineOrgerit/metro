58c5849811f6238f1c531471c51985ea5e5d9717
Tibo28L <thibaud.lambert@etudiant.univ-lr.fr>
2018-12-04 14:57:25 +0100

    Ajout des noms de stations

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
-$(".dropdown-trigger").dropdown();
-loadStations();
-
 let stationsList = {};
+let stationsLoadedEvent = new Event("stationsLoaded");
+
+$(document).ready(init);
+
+function init() {
+    //Ecouter événement
+    document.addEventListener("stationsLoaded", loadDropdown);
+
+    $(".dropdown-trigger").dropdown();
+    loadStations();
+    loadDropdown();
+}
 
 /**
  *
@@ -25,6 +34,31 @@ function loadStations() {
                 }
             }
         }
+        document.dispatchEvent(stationsLoadedEvent);
     });
 }
 
+function loadDropdown() {
+    var ulHtml = "";
+    var tmp = "";
+
+    for (var station in stationsList) {
+        tmp = "<li class='dropdownItem'><a href=\"#!\">" + station + "</a></li><li class=\"divider\" tabindex=\"-1\"></li>";
+        ulHtml = ulHtml + tmp;
+    }
+
+    $("#dropdownDep").html(ulHtml);
+    $("#dropdownArr").html(ulHtml);
+
+    //Tap sur le dropdown de depart
+    $("#dropdownDep .dropdownItem").click(function() {
+        let element = $(this).children();
+        $("#dropdownDepTitle").html(element.text());
+    });
+
+    //Tap sur le dropdown d'arrivee
+    $("#dropdownArr .dropdownItem").click(function() {
+        let element = $(this).children();
+        $("#dropdownArrTitle").html(element.text());
+    });
+}
