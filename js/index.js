91ed3c13d529a320dc9d6f6ca0a910740ccc39cf
Florian <florian.bertonnier@etudiant.univ-lr.fr>
2018-12-04 12:00:53 +0100

    ajout graphe et data.json

--- js/index.js
+++ js/index.js
@@ -1,21 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 
-    $.ajax({
-        url: "",
-        type: "POST",
-        dataType: "xml",
-        data: "",
-        processData: false,
-        contentType: "text/xml; charset=\"utf-8\"",
-        success: function () {
-
-        },
-        error: function () {
-
-        }
-    });
-
-    // jQuery methods go here...
-    console.log("jquery");
-});
+}
