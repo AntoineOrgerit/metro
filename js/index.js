6e00ab5f114302fca22f3ac65beb13a4762d2ff2
alexandrebaret <alexandre.baret1@etudiant.univ-lr.fr>
2018-12-04 13:54:33 +0100

    Ajout d'une map pour afficher le trajet + formattage données

--- js/index.js
+++ js/index.js
@@ -1,4 +1,33 @@
-$(document).ready(function() {
+$(".dropdown-trigger").dropdown();
 
+let stationsList = {};
 
+/**
+ *
+ */
+function loadStations() {
+    fetch("json/data.json").then(function (response) {
+        return response.text();
+    }).then(function (data) {
+        if (data !== undefined) {
+            var dataReceived = JSON.parse(data);
+            var stations = dataReceived.ligne["11"];
+
+            if (stations !== undefined) {
+                stations = stations[0];
+
+                for (var station in stations) {
+                    if (stationsList[] === undefined) {
+                        stationsList[station] = {};
+                        stationsList[station].etapes = stations[station];
+                    }
+                }
+            }
+        }
+    });
 }
+
+
+
+loadStations();
+
