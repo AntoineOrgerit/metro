bbf06a4f7853d5f59e918bc6eb2a454c0b94744a
Florian <florian.bertonnier@etudiant.univ-lr.fr>
2018-12-04 10:14:44 +0100

    Ajout ajax

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
+
+    $.ajax({
+        url: "",
+        type: "POST",
+        dataType: "xml",
+        data: "",
+        processData: false,
+        contentType: "text/xml; charset=\"utf-8\"",
+        success: function () {
+
+        },
+        error: function () {
+
+        }
+    });
+    
     // jQuery methods go here...
     console.log("jquery");
 });
