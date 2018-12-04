48f112a421afee783a982d2ca912945b210a6044
Tibo28L <thibaud.lambert@etudiant.univ-lr.fr>
2018-12-04 17:42:58 +0100

    Afichage par tri alphabetique

--- js/index.js
+++ js/index.js
@@ -32,11 +32,9 @@ function loadStations() {
             var stations = dataReceived.stations;
 
             if (stations !== undefined) {
-                for (var station in stations) {
-                    if (stationsList[station] === undefined) {
-                        stationsList[station] = stations[station];
-                    }
-                }
+                Object.keys(stations).sort().forEach(function(key) {
+                    stationsList[key] = stations[key];
+                });
             }
         }
         document.dispatchEvent(stationsLoadedEvent);
@@ -107,9 +105,9 @@ function loadItinerary() {
         let tmp = "";
 
         if (etapes !== null) {
-            for (var i =0;i<etapes.length;i++) {
+            for (var i = 0; i < etapes.length; i++) {
                 if (i !== 0 && i !== etapes.length - 1) {
-                    tmp =  "<li style='list-style-type: circle'>" + etapes[i] + "</li>";
+                    tmp = "<li style='list-style-type: circle'>" + etapes[i] + "</li>";
                     liHtml = liHtml + tmp;
                 }
 
@@ -117,22 +115,25 @@ function loadItinerary() {
 
             stepListElement.html(liHtml);
 
+            if (etapes !== null) {
+                $("#time").html(etapes.length * 2 - 2 + " min");
+            }
 
-            $("#time").html(etapes.length * 2 - 2 + " min");
+
             $("#result").css('display', "block");
+
+        } else {
+            let stack = "Impossible de charger l'itinéraire.";
+
+            if (error === "StationDepStationArr") {
+                stack += " Les stations sélectionnées sont incorrectes"
+            } else if (error === "StationDep") {
+                stack += " La station de départ est incorrecte";
+            } else if (error === "StationArr") {
+                stack += " La station de d'arrivée est incorrecte";
+            }
+            M.toast({html: stack});
         }
 
-    } else {
-        let stack ="Impossible de charger l'itinéraire.";
-
-        if (error === "StationDepStationArr") {
-            stack += " Les stations sélectionnées sont incorrectes"
-        } else if (error === "StationDep") {
-            stack += " La station de départ est incorrecte";
-        } else if (error === "StationArr") {
-            stack += " La station de d'arrivée est incorrecte";
-        }
-        M.toast({html: stack});
     }
-
 }
