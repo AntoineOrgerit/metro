ab23c506d829946399c74480bee609c04d211f14
Jessy Barritault <jessy.barritault@etudiant.univ-lr.fr>
2018-12-04 10:22:20 +0100

    Revert "Ajout fonction ajax"
    This reverts commit bb380ebf

--- js/index.js
+++ js/index.js
@@ -1,20 +1,5 @@
 $(document).ready(function(){
 
-
-    $.ajax({
-        url: "",
-        type: "POST",
-        dataType: "xml",
-        data: "",
-        processData: false,
-        contentType: "text/xml; charset=\"utf-8\"",
-        success: function () {
-
-        },
-        error: function () {
-
-        }
-    });
     console.log("jquery");
 });
 
