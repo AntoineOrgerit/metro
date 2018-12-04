d5b6f4eb977ae99635aadf35bddc2a02e7658231
Jessy Barritault <jessy.barritault@etudiant.univ-lr.fr>
2018-12-04 17:38:54 +0100

    modification graph pour avoir le numero de ligne

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
  $.getJSON("json/data.json",function (data) {
     console.log(data);
-     var graph = new Graph(data.ligne);
+     var graph = new Graph(data.graph);
      console.log(graph.findShortestPath("Chatelet", 'Rambuteau'));
  });
 
